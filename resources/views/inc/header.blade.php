f1645ed71f2ff07fa7dfb5561624d29d9e0e9151
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2020-12-28 18:26:43 +0300

    Header v1.0.1

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,6 +1,6 @@
 <header>
-  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #33a0ff;">
-    <a class="navbar-brand" href="#">Rhythmicgym</a>
+  <nav class="navbar navbar-expand-lg navbar-light nav-pills nav-fill" style="background-color: #33a0ff;">
+    <a class="navbar-brand" href="#"><h3>Rhythmicgym</h3></a>
     <a class="nav-link" href="#">Active</a>
     <a class="nav-link" href="#">Link</a>
     <a class="nav-link" href="#">Link</a>
