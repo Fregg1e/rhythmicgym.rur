d412af587779bb32408c06e6c072f1aad0c3e106
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2021-01-02 20:00:06 +0300

    Mod header 01

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,4 +1,13 @@
-<header>
+<header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <a class="h2 my-0 me-md-auto fw-normal text-dark" href="#">Rhythmicgym</a>
+  <nav class="my-2 my-md-0 me-md-3">
+    <a class="p-2 text-dark" href="#">Каталог</a>
+    <a class="p-2 text-dark" href="#">О нас</a>
+    <a class="p-2 text-dark" href="#">Контакты</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="#">Sign up</a>
+</header>
+<!-- <header>
   <nav class="navbar navbar-expand-lg navbar-light">
     <a class="navbar-brand" href="/"><h3>Rhythmicgym</h3></a>
     <a class="nav-link" href="/">Главная</a>
@@ -6,4 +15,4 @@
     <a class="nav-link" href="/about">О нас</a>
     <a class="nav-link" href="contacts">Контакты</a>
   </nav>
-</header>
+</header> -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title-block')</title>
+  <link rel="stylesheet" href="/css/app.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
         rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
         crossorigin="anonymous">
-  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
-  <link rel="stylesheet" href="/css/app.css">
-  </head>
+</head>
   <body>
     @include('inc.header')
     @yield('content')
