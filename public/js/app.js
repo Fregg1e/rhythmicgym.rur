4a447c6c25f6ea7d1657c9c33028334ce6d4493f
Alexandr Shalaginov <alxshlgnv@yandex.ru>
2021-01-03 18:20:01 +0300

    Seed data into DB

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\WebDev\OpenServer\OpenServer\domains\rhythmicgym.ru\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\WebDev\OpenServer\OpenServer\domains\rhythmicgym.ru\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\webdev\openserver\openserver\domains\rhythmicgym.ru\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\webdev\openserver\openserver\domains\rhythmicgym.ru\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
